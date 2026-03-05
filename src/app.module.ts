614e681eb695ba2356e5511523c69608f65a0c4e
Neid-09 <neiderqm09@gmail.com>
2026-03-04 19:26:01 -0500

    feat: Configuracion inicial para conexion a base de datos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

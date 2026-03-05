ec6d3f9ba6a596793c96c846f94603d12670bc9d
Neid-09 <neiderqm09@gmail.com>
2026-03-04 20:46:10 -0500

    feat: Agregar soporte para TypeORM en los módulos de Beneficios, Cargos, Contratos, Departamentos y Empleados

--- src/beneficios/beneficios.module.ts
+++ src/beneficios/beneficios.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BeneficiosService } from './beneficios.service';
 import { BeneficiosController } from './beneficios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Beneficio } from './entities/beneficio.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Beneficio])],
   controllers: [BeneficiosController],
   providers: [BeneficiosService],
 })
--- src/departamentos/departamentos.module.ts
+++ src/departamentos/departamentos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DepartamentosService } from './departamentos.service';
 import { DepartamentosController } from './departamentos.controller';
+import { Departamento } from './entities/departamento.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Departamento])],
   controllers: [DepartamentosController],
   providers: [DepartamentosService],
 })

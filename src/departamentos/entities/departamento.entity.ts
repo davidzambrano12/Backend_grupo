05835f07fe6114ca60d7714d845549a2eab49f30
Neid-09 <neiderqm09@gmail.com>
2026-03-04 19:43:35 -0500

    fear: Entidad departamento

--- src/departamentos/entities/departamento.entity.ts
+++ src/departamentos/entities/departamento.entity.ts
@@ -1,1 +1,11 @@
-export class Departamento {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Departamento {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({type: 'varchar', length: 80})
+  name: string;
+}
